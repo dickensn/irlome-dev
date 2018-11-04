5001b07e7dc6b3109b4926a3defd1fdb9c83af23
Nick Dickens <dickensn@fau.edu>
2018-11-03 21:39:06 -0400

    current web - not working

--- web/app.js
+++ web/app.js
@@ -13,6 +13,10 @@ function clickHandler() {
   alert("something");
 }
 
+function get_file_extension(filename){
+  return filename.split('.').pop();
+}
+
 function addPhoto() {
   var files = document.getElementById('photoupload').files;
   if (!files.length) {
@@ -34,13 +38,20 @@ function addPhoto() {
 // add submit to API instead?
 });*/
 
-  var photoKey = "photos/" + file
+  var date = new Date();
+  var timestamp = date.getTime();
+
+  var extension = get_file_extension(fileName)
+
+  var photoKey = "/photos/" + timestamp + "." + extension
+
 
   var params = {
   Body: file,
   Bucket: bucketName,
   Key: photoKey
  };
+ //putObject
  s3.putObject(params, function(err, data) {
    if (err) console.log(err, err.stack); // an error occurred
    else     console.log(data);           // successful response
@@ -55,3 +66,25 @@ function addPhoto() {
      <p>To see the data we have, please visit the map.</p> \
    </div>";
  }
+
+ var upload_image = function(url,form,id) {
+    var field = $(form).find('input[name=photo]');
+    var file = field[0].files[0];
+    var original_name = field.val();
+    var extension = original_name.substr((original_name.lastIndexOf('.')));
+    var filename = id + extension;
+
+    var fd = new FormData();
+    fd.append('key', filename);
+    fd.append('acl', 'bucket-owner-full-control');
+    fd.append('Content-Type', file.type);
+    fd.append("file",file);
+
+    return $.ajax({
+        type : 'POST',
+        url : url,
+        data : fd,
+        processData: false,  // tell jQuery not to convert to form data
+        contentType: false  // tell jQuery not to set contentType
+    });
+};
